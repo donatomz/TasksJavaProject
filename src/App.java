9476308cb2da26ce8bde1fc7a22edd7f6119c48f
donatomz <donato.zinni@gmail.com>
2024-06-13 06:19:22 -0700

    added spring boot functionality

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         System.out.println("Hello, World!!!");
         System.out.println("Tasks Project!!");
     }
